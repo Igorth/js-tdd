d3322ba61156a71320bd9b9a699bb1463bedd3bd
Igorth <igordiasth@gmail.com>
2020-03-17 11:47:20 +1300

    describe, context and it

---
+++ tests/main.spec.js
@@ -0,0 +1,21 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen something', () => {
+        // espera o que aconteça
+        // espera de dados / metodo sum(2,2)
+        // espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', () => {
+      it('should happen otherthing', () => {
+
+      });
+    });
+  });
+
+  describe('Method B', () => {
+
+  });
+});
