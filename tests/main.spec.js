a72b112df8c9e49e7b28fa1f861b6be7c3934abe
Igorth <igordiasth@gmail.com>
2020-03-18 10:41:32 +1300

    hoocks in practice

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -28,31 +28,40 @@
 
 
 describe('Main', () => {
+  let arr;
   // roda uma vez, antes do bloco
   before(() => {
-    console.log('before');
+    // iniciar a conexao ao banco de dados
+    // criar um conjunto de dados
   });
 
   // roda uma vez, depois do bloco
   after(() => {
-    console.log('after');
+    // encerrar a conexao ao banco de dados
+    // apagar o conjunto de dados
   });
 
   // roda todas as vez, antes de CADA bloco
   beforeEach(() => {
-    console.log('beforeEach');
+    arr = [1, 2, 3];
   });
 
   // roda todas as vez, depois de CADA bloco
   afterEach(() => {
-    console.log('afterEach');
+
   });
 
-  it('test 1', () => {
-    console.log('test 1');
+  it('should have a size of 4 when push another valueto the array', () => {
+    arr.push(4);
+    console.log(arr.length);
   });
 
-  it('test 2', () => {
-    console.log('test 2');
+  it('should have a size of 2 when pop a value from the array', () => {
+    arr.pop();
+    console.log(arr.length);
+  });
+
+  it('should remove the value 3 when use pop in the array', () => {
+    console.log(arr.pop() === 3);
   });
 });
