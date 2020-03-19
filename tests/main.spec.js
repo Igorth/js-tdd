a3fbf3deea1e114ff21a1999cd09e14ab0daae47
Igorth <igordiasth@gmail.com>
2020-03-19 15:44:40 +1300

    chai methods

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -52,6 +52,11 @@ describe('Main', () => {
 
   });
 
+  // testar tipo e se existe, (smoke test)
+  it('should be an array', () => {
+    expect(arr).to.be.an('array');
+  });
+
   it('should have a size of 4 when push another valueto the array', () => {
     arr.push(4);
     expect(arr).to.have.lengthOf(4);
@@ -60,6 +65,10 @@ describe('Main', () => {
   it('should have a size of 2 when pop a value from the array', () => {
     arr.pop();
     expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop 3 from the array', () => {
+    expect(arr.pop() === 3).to.be.true;
   });
 
   it('should remove the value 3 when use pop in the array', () => {
