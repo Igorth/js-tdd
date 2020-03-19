0c1d819a263b4ca2351beb9af2d1c0898b7349b0
Igorth <igordiasth@gmail.com>
2020-03-19 19:19:41 +1300

    creating a simple cal, smoke tests

---
+++ tests/calc.spec.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+const { expect } = require('chai');
+const calc = require('../src/calc.js');
+
+describe('Calc', () => {
+  // smoke tests
+  describe('Smoke tests', () => {
+    it('should exist the calc lib', () => {
+      expect(calc).to.exist;
+    });
+    it('should exist the method `sum`', () => {
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a('function');
+    });
+    it('should exist the method `sub`', () => {
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a('function');
+    });
+    it('should exist the method `mult`', () => {
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a('function');
+    });
+    it('should exist the method `div`', () => {
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a('function');
+    });
+  });
+});
