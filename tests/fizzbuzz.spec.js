7b6d92d0e3c033e27d4dcef3c810706d3fcc398d
Igorth <igordiasth@gmail.com>
2020-03-21 09:02:46 +1300

    fizzbuzz with tdd

---
+++ tests/fizzbuzz.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/*
+Escreva uma lib que receba um numero e.
+
+Se o numero for divisivel por 3, no lugar do numero escreva `Fizz`;
+Se o numero for divisivel por 5, no lugar do numero escreva `Buzz`;
+Se o numero for divisivel por 3 e 5, no lugar do numero escreva `FizzBuzz`;
+Se nao for multiplo de nada, retorna o numero
+
+*/
+
+import { expect } from 'chai';
+
+describe('FizzBuzz', () => {
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(Fizzbuzz(3)).to.be.equal('Fizz');
+  });
+});
