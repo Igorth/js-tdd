51eec0ef401dea593703bd2881afe120e4a808af
Igorth <igordiasth@gmail.com>
2020-03-21 09:24:35 +1300

    test fizzbuzz

--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -22,4 +22,9 @@ describe('FizzBuzz', () => {
     expect(FizzBuzz(5)).to.be.equal('Buzz');
     expect(FizzBuzz(10)).to.be.equal('Buzz');
   });
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
+  });
 });
