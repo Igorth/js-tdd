774ffcd38a6c63396fe6eaedc779c4ad2f76cad5
Igorth <igordiasth@gmail.com>
2020-03-21 09:16:11 +1300

    Buzz test tdd

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,10 @@ const FizzBuzz = (num) => {
   if (num % 3 === 0) {
     return 'Fizz';
   }
+
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
 };
 
 export default FizzBuzz;
--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -15,5 +15,11 @@ import FizzBuzz from '../src/fizzbuzz';
 describe('FizzBuzz', () => {
   it('should return `Fizz` when multiple of 3', () => {
     expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
   });
 });
